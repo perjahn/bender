perjahn/bender

commit 69167602e94a1bc5f56fafaaf771962e1e02ae54

Fixed problematic file tailing.

FileTailer.cs
@@ -150,6 +150,7 @@ namespace Bender
                         if (local == 0)
                         {
                             eol = cr;
+                            --absolutePos;
                         }
                         else if (buf[i] == 13)
                         {
@@ -190,7 +191,7 @@ namespace Bender
                     }
                 }
 
-                if (readFromStart || lines.Count > lc * 2)
+                if (readFromStart || lines.Count >= lc * 2)
                 {
                     // Read whole file or found all lines
                     break;

commit 99d8ba8d0e6d7ae2415bebda34d57121d01c031d

Fix issues with tailing introduced in 69167602e94a1bc5f56fafaaf771962e1e02ae54

FileTailer.cs
@@ -191,9 +191,9 @@ namespace Bender
                     }
                 }
 
-                if (readFromStart || lines.Count >= lc * 2)
+                if (readFromStart || lc == 0 || lines.Count > lc * 2)
                 {
-                    // Read whole file or found all lines
+                    // Read whole file or found all lines. For the third exit condition, note that partial lines are considered, so more lines than expected need to be read.
                     break;
                 }
 