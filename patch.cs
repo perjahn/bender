b509751ad1b4a24e1734f50fd40a8710213f11db
Per Jahn <perjahn@gmail.com>
2016-09-15 16:54:08 +0200

    Patch: Fixed download, installation and reboot.

--- patch.cs
+++ patch.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -11,10 +13,47 @@ namespace Bender
 {
     class Patch
     {
+        private struct LUID
+        {
+            public int LowPart;
+            public int HighPart;
+        }
+
+        private struct LUID_AND_ATTRIBUTES
+        {
+            public LUID pLuid;
+            public int Attributes;
+        }
+
+        private struct TOKEN_PRIVILEGES
+        {
+            public int PrivilegeCount;
+            public LUID_AND_ATTRIBUTES Privileges;
+        }
+
+        [DllImport("advapi32.dll")]
+        static extern int OpenProcessToken(IntPtr ProcessHandle, int DesiredAccess, out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AdjustTokenPrivileges(IntPtr TokenHandle,
+            [MarshalAs(UnmanagedType.Bool)]bool DisableAllPrivileges,
+            ref TOKEN_PRIVILEGES NewState,
+            UInt32 BufferLength,
+            IntPtr PreviousState,
+            IntPtr ReturnLength);
+
+        [DllImport("advapi32.dll")]
+        static extern int LookupPrivilegeValue(string lpSystemName, string lpName, out LUID lpLuid);
+
         [DllImport("user32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool ExitWindowsEx(int uFlags, int dwReason);
 
+        const string SE_SHUTDOWN_NAME = "SeShutdownPrivilege";
+        const short SE_PRIVILEGE_ENABLED = 2;
+        const short TOKEN_ADJUST_PRIVILEGES = 32;
+        const short TOKEN_QUERY = 8;
 
         public void InstallPatches(bool rebootIfNeeded, bool onlyList, Stream output)
         {
@@ -53,22 +92,30 @@ namespace Bender
             {
                 Bender.WriteLine("Retrieving patches from: " + service.Name, output);
 
-                var searcher = session.CreateUpdateSearcher();
-                searcher.ServerSelection = ServerSelection.ssWindowsUpdate;
-                searcher.ServiceID = service.ServiceID;
-
-                ISearchResult searchresult = searcher.Search("");
-
-                UpdateCollection updatecollection = searchresult.Updates;
-
-                Bender.WriteLine("Found " + updatecollection.Count + " updates.", output);
-
-                foreach (IUpdate5 update in updatecollection)
+                try
                 {
-                    if (!updates.Any(u => u.Title == update.Title))
+                    var searcher = session.CreateUpdateSearcher();
+                    searcher.ServerSelection = ServerSelection.ssWindowsUpdate;
+                    searcher.ServiceID = service.ServiceID;
+
+                    ISearchResult searchresult = searcher.Search("");
+
+                    UpdateCollection updatecollection = searchresult.Updates;
+
+                    Bender.WriteLine("Found " + updatecollection.Count + " updates.", output);
+
+                    foreach (IUpdate5 update in updatecollection)
                     {
-                        updates.Add(update);
+                        if (!updates.Any(u => u.Title == update.Title))
+                        {
+                            updates.Add(update);
+                        }
                     }
+                }
+                catch (COMException ex)
+                {
+                    Bender.WriteLine("Couldn't retrive patches: 0x" + ex.HResult.ToString("X"), output);
+                    Bender.WriteLine(ex.ToString(), output);
                 }
             }
 
@@ -101,34 +148,37 @@ namespace Bender
                     Bender.WriteLine("Patch is already downloaded: " + update.Title, output);
                     continue;
                 }
-                else
+
+
+                UpdateCollection updateCollection = new UpdateCollection();
+                updateCollection.Add(update);
+
+                UpdateDownloader downloader = session.CreateUpdateDownloader();
+                downloader.Updates = updateCollection;
+
+                bool downloaded = false;
+
+                for (int tries = 0; tries < 3 && !downloaded; tries++)
                 {
-                    UpdateDownloader downloader = session.CreateUpdateDownloader();
-
-                    bool downloaded = false;
-
-                    for (int tries = 0; tries < 3 && !downloaded; tries++)
+                    try
                     {
                         string printtry = tries > 0 ? " (try " + (tries + 1) + ")" : string.Empty;
 
                         Bender.WriteLine("Downloading" + printtry + ": " + update.Title + ": " + GetPrintSize(update.MaxDownloadSize) + " MB.", output);
 
-                        try
+                        IDownloadResult downloadresult = downloader.Download();
+                        if (downloadresult.ResultCode == OperationResultCode.orcSucceeded)
                         {
-                            IDownloadResult downloadresult = downloader.Download();
-                            if (downloadresult.ResultCode == OperationResultCode.orcSucceeded)
-                            {
-                                downloaded = true;
-                            }
-                            else
-                            {
-                                Bender.WriteLine("Couldn't download patch: " + downloadresult.ResultCode + ": 0x" + downloadresult.HResult.ToString("X"), output);
-                            }
+                            downloaded = true;
                         }
-                        catch (COMException ex) when ((uint)ex.HResult == (uint)0x80240004)
+                        else
                         {
-                            Bender.WriteLine("Couldn't download patch: 0x" + ex.HResult.ToString("X"), output);
+                            Bender.WriteLine("Couldn't download patch: " + downloadresult.ResultCode + ": 0x" + downloadresult.HResult.ToString("X"), output);
                         }
+                    }
+                    catch (COMException ex)
+                    {
+                        Bender.WriteLine("Couldn't download patch: 0x" + ex.HResult.ToString("X"), output);
                     }
                 }
             }
@@ -153,23 +203,56 @@ namespace Bender
                 }
                 else
                 {
-                    IUpdateInstaller installer = session.CreateUpdateInstaller();
-                    Bender.WriteLine("Installing: " + update.Title, output);
-                    IInstallationResult installresult = installer.Install();
-
-                    if (installresult.RebootRequired)
+                    try
                     {
-                        reboot = true;
+                        Bender.WriteLine("Installing: " + update.Title, output);
+
+                        UpdateCollection updateCollection = new UpdateCollection();
+                        updateCollection.Add(update);
+
+                        IUpdateInstaller installer = session.CreateUpdateInstaller();
+                        installer.Updates = updateCollection;
+
+                        IInstallationResult installresult = installer.Install();
+                        if (installresult.ResultCode == OperationResultCode.orcSucceeded)
+                        {
+                            if (installresult.RebootRequired)
+                            {
+                                reboot = true;
+                            }
+                        }
+                        else
+                        {
+                            Bender.WriteLine("Couldn't install patch: " + installresult.ResultCode + ": 0x" + installresult.HResult.ToString("X"), output);
+                        }
+                    }
+                    catch (COMException ex)
+                    {
+                        Bender.WriteLine("Couldn't download patch: 0x" + ex.HResult.ToString("X"), output);
                     }
                 }
             }
 
-            if (reboot)
+            string regpath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired";
+            if (reboot || CheckIfLocalMachineKeyExists(regpath))
             {
                 if (rebootIfNeeded)
                 {
                     Bender.WriteLine("Rebooting.", output);
-                    ExitWindowsEx(6, 0);
+
+                    IntPtr hToken;
+                    TOKEN_PRIVILEGES tkp;
+
+                    OpenProcessToken(Process.GetCurrentProcess().Handle, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, out hToken);
+                    tkp.PrivilegeCount = 1;
+                    tkp.Privileges.Attributes = SE_PRIVILEGE_ENABLED;
+                    LookupPrivilegeValue("", SE_SHUTDOWN_NAME, out tkp.Privileges.pLuid);
+                    AdjustTokenPrivileges(hToken, false, ref tkp, 0U, IntPtr.Zero, IntPtr.Zero);
+
+                    if (!ExitWindowsEx(6, 0))
+                    {
+                        Bender.WriteLine("Couldn't reboot.", output);
+                    }
                 }
                 else
                 {
@@ -181,6 +264,20 @@ namespace Bender
         string GetPrintSize(decimal size)
         {
             return size > 0 && (int)(size / 1024 / 1024) == 0 ? "<1" : ((int)(size / 1024 / 1024)).ToString();
+        }
+
+        private bool CheckIfLocalMachineKeyExists(string regpath)
+        {
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(regpath);
+            if (key != null)
+            {
+                key.Close();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
